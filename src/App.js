bf2b33654a7c9fbb7e15bf0eb02b6ac9ccc819d3
Success0406 <success@nynthlabs.com>
2023-05-22 21:34:00 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./index.css";
+import Navbar from "./components/navbar/navbar.jsx";
+/* The following line can be included in a src/App.scss */
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <Navbar />;
 }
 
 export default App;
